89ccd4d1a1be888834ef7eafbb94001b9b6ffb36
Francy Valencia <francy@MacBook-Pro-de-Francy.local>
2024-06-20 19:57:20 -0500

    index.html

---
+++ burguermenu.js
@@ -0,0 +1,16 @@
+// script.js
+document.addEventListener("DOMContentLoaded", () => {
+    const burger = document.querySelector(".burger");
+    const mobileMenu = document.querySelector(".mobile-menu");
+    const menuLinks = document.querySelectorAll(".mobile-menu a");
+
+    burger.addEventListener("click", () => {
+        mobileMenu.classList.toggle("show");
+    });
+
+    menuLinks.forEach(link => {
+        link.addEventListener("click", () => {
+            mobileMenu.classList.remove("show");
+        });
+    });
+});
